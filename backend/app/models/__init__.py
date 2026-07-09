7d472accee7abc7cea035f8a179b6503097c5e0e
DLuanNeves <didimoluan@yahoo.com.br>
2026-07-09 15:59:01 -0300

    pasta models

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.usuario import Usuario
+from app.models.loja import Loja
+from app.models.produto import Produto
+from app.models.venda import Venda
+from app.models.cliente import Cliente
